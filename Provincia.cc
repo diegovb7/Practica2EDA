9a464789b41a0796cd5a9de439b86fdae2dfdf48
DiegoValero12 <diego.valero12@gmail.com>
2019-11-08 13:14:54 +0100

    Hasta calculaCercanas

--- Provincia.cc
+++ Provincia.cc
@@ -4,4 +4,34 @@
 #include "Provincia.h"
 
 
+Provincia::Provincia(){
+    lc=Localidad();
+    locprox=LNear();
+}
 
+Provincia::Provincia(Localidad l){
+    lc=l;
+    locprox=LNear();
+}
+
+Provincia::Provincia(const Provincia &p){
+    lc=p.lc;
+    locprox=p.locprox;
+}
+
+Provincia & Provincia::operator=(const Provincia &p){
+    if(this!=&p){
+        lc=p.lc;
+        locprox=p.locprox;
+    }
+    return *this;
+}
+
+Provincia::~Provincia(){
+    lc=Localidad();
+    locprox=LNear();
+}
+
+void Provincia::calculaCercanas(Coleccion &c, int distancia){
+    
+}
