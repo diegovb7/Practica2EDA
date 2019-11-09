e6b5ab1512e391d687fc94743680d08c92600372
DiegoValero12 <diego.valero12@gmail.com>
2019-11-09 20:06:28 +0100

    Cambios hasta ordenar el vector de las localidades costeras

--- Provincia.cc
+++ Provincia.cc
@@ -32,6 +32,79 @@ Provincia::~Provincia(){
     locprox=LNear();
 }
 
+int Provincia::calculaDistancia(Localidad a, Localidad b){
+    int distancia=0;
+    distancia=abs(a.getCoor().getFila()-b.getCoor().getFila())+abs(a.getCoor().getColumna()-b.getCoor().getColumna());
+    return distancia;
+}
+
 void Provincia::calculaCercanas(Coleccion &c, int distancia){
-    
+    if(!locprox.esVacia()){
+        locprox.borraLocalidades(-1);
+    }
+    else{
+        for(unsigned int i=0;i<c.getLocalidades().size();i++){
+            int nueva_distancia=calculaDistancia(lc, c.getLocalidades()[i]);
+            if(nueva_distancia<=distancia){
+                locprox.insertaLocalidad(c.getLocalidades()[i], nueva_distancia);
+            }
+        }
+    }
+}
+
+int Provincia::borraLocalidad(string s){
+    int ret;
+    ret=locprox.borraLocalidad(s);
+    return ret;
+}
+
+LNear & Provincia::getCercanas(){
+    return locprox;
+}
+
+bool Provincia::esCostera(Coleccion &c, Localidad l){
+    bool ret=false;
+    if(c.getMapa()[l.getCoor().getFila()-1][l.getCoor().getColumna()]=='M' ||
+        c.getMapa()[l.getCoor().getFila()+1][l.getCoor().getColumna()]=='M' || 
+        c.getMapa()[l.getCoor().getFila()][l.getCoor().getColumna()-1]=='M' || 
+        c.getMapa()[l.getCoor().getFila()][l.getCoor().getColumna()+1]=='M'){
+        ret=true;
+    }
+    return ret;
+}
+
+vector<Localidad> Provincia::vectorCosteras(Coleccion &c){
+    vector<Localidad> costeras;
+    for(unsigned int i=0;i<c.getLocalidades().size();i++){
+        if(esCostera(c, c.getLocalidades()[i])){
+            costeras.push_back(c.getLocalidades()[i]);
+        }
+    }
+    return costeras;
+}
+
+/* METODO DE ORDENACION DEL VECTOR DEPENDIENDO DE LA DISTANCIA A LA CAPITAL
+void ordenarArray(tArray miArray){
+	float temporal;
+	
+	for (int i = 0;i < DIM; i++){
+		for (int j = 0; j< DIM-1; j++){
+			if (miArray[j] < miArray[j+1]){ // Ordena el array de mayor a menor, cambiar el "<" a ">" para ordenar de menor a mayor
+			temporal = miArray[j]; 
+			miArray[j] = miArray[j+1]; 
+			miArray[j+1] = temporal;
+			}
+		}
+	}
+}
+*/
+
+string Provincia::getCostera(Coleccion &c){
+    vector<Localidad> costeras=vectorCosteras(c);
+    if(costeras.empty()){
+        return "no hay ninguna localidad costera";
+    }
+    else{
+        
+    }
 }
--- Provincia.h
+++ Provincia.h
@@ -4,6 +4,7 @@ class Provincia{
     private:
         Localidad lc;
         LNear locprox;
+        LNear costeras;
 
     public:
         Provincia();
@@ -11,9 +12,12 @@ class Provincia{
         Provincia(const Provincia &);
         ~Provincia();
         Provincia & operator=(const Provincia &);
+        int calculaDistancia(Localidad, Localidad );
         void calculaCercanas(Coleccion &, int );
         int borraLocalidad(string );
         LNear & getCercanas();
+        bool esCostera(Coleccion &, Localidad );
+        vector<Localidad> vectorCosteras(Coleccion &);
         string getCostera(Coleccion & );
         LNear getCosteras(Coleccion & );
         string getConAeropuerto();
