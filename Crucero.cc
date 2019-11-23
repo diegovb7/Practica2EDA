7d53f2f22ad7ba4a378a4e2f15e5ed8d92796829
DiegoValero12 <diego.valero12@gmail.com>
2019-11-23 01:49:49 +0100

    Crucero iniciado con vectores, algoritmos de busqueda para cada vista hecho, queda implantar en main

--- Crucero.cc
+++ Crucero.cc
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 int vista0(Coleccion &, Provincia &, Coordenadas &);
+/*
 int vista1(Coleccion &, Provincia &, Coordenadas &);
 int vista2(Coleccion &, Provincia &, Coordenadas &);
 int vista3(Coleccion &, Provincia &, Coordenadas &);
@@ -11,6 +12,11 @@ int vista4(Coleccion &, Provincia &, Coordenadas &);
 int vista5(Coleccion &, Provincia &, Coordenadas &);
 int vista6(Coleccion &, Provincia &, Coordenadas &);
 int vista7(Coleccion &, Provincia &, Coordenadas &);
+*/
+vector<Coordenadas> creaVista0(vector<Coordenadas> &);
+
+
+void cambiarCoordenadas(Coordenadas &, Coordenadas &, Coordenadas &, Coordenadas &, Coordenadas &, Coordenadas &, Coordenadas &, Coordenadas &, Coordenadas &);
 
 
 int main(int argc, char *argv[]){
@@ -50,62 +56,35 @@ int main(int argc, char *argv[]){
 
     //ahora que ya tenemos la localidad de partida del crucero, tenemos que ver cual va a ser la primera direccion
     
-    Coordenadas d0(inicio.getLocalidad().getCoor().getFila(), inicio.getLocalidad().getCoor().getColumna()+1);
-    Coordenadas d1(inicio.getLocalidad().getCoor().getFila()+1, inicio.getLocalidad().getCoor().getColumna()+1);
-    Coordenadas d2(inicio.getLocalidad().getCoor().getFila()+1, inicio.getLocalidad().getCoor().getColumna());
-    Coordenadas d3(inicio.getLocalidad().getCoor().getFila()+1, inicio.getLocalidad().getCoor().getColumna()-1);
-    Coordenadas d4(inicio.getLocalidad().getCoor().getFila(), inicio.getLocalidad().getCoor().getColumna()-1);
-    Coordenadas d5(inicio.getLocalidad().getCoor().getFila()-1, inicio.getLocalidad().getCoor().getColumna()-1);
-    Coordenadas d6(inicio.getLocalidad().getCoor().getFila()-1, inicio.getLocalidad().getCoor().getColumna());
-    Coordenadas d7(inicio.getLocalidad().getCoor().getFila()-1, inicio.getLocalidad().getCoor().getColumna()+1);
+    Coordenadas d0;
+    Coordenadas d1;
+    Coordenadas d2;
+    Coordenadas d3;
+    Coordenadas d4;
+    Coordenadas d5;
+    Coordenadas d6;
+    Coordenadas d7;
+    cambiarCoordenadas(d0, d1, d2, d3, d4, d5, d6, d7, inicio.getLocalidad().getCoor());
     int direccion_inicial=0;
     Coordenadas nuevaCoordenada;
-
-    if(c.getCoorMapa(d0)=='M' || (c.getCoorMapa(d1)=='L' || c.getCoorMapa(d1)=='T')){
-        direccion_inicial=1;
-        nuevaCoordenada=d1;
+    vector<Coordenadas> vector_inicial;
+    vector_inicial.push_back(d0);
+    vector_inicial.push_back(d1);
+    vector_inicial.push_back(d2);
+    vector_inicial.push_back(d3);
+    vector_inicial.push_back(d4);
+    vector_inicial.push_back(d5);
+    vector_inicial.push_back(d6);
+    vector_inicial.push_back(d7);
+    bool coordenada_encontrada=false;
+    for(unsigned int i=1;i<=vector_inicial.size() && !coordenada_encontrada;i++){
+        if(c.getCoorMapa(vector_inicial[i-1])=='M' && (c.getCoorMapa(vector_inicial[i])=='T' || c.getCoorMapa(vector_inicial[i])=='L')){
+            direccion_inicial=i;
+            coordenada_encontrada=true;
+            nuevaCoordenada=vector_inicial[i];
+        }
     }
-    else{
-        if(c.getCoorMapa(d1)=='M' || (c.getCoorMapa(d2)=='L' || c.getCoorMapa(d2)=='T')){
-            direccion_inicial=2;
-            nuevaCoordenada=d2;
-        }
-        else{
-            if(c.getCoorMapa(d2)=='M' || (c.getCoorMapa(d3)=='L' || c.getCoorMapa(d3)=='T')){
-                direccion_inicial=3;
-                nuevaCoordenada=d3;
-            }
-            else{
-                if(c.getCoorMapa(d3)=='M' || (c.getCoorMapa(d4)=='L' || c.getCoorMapa(d4)=='T')){
-                    direccion_inicial=4;
-                    nuevaCoordenada=d4;
-                }
-                else{
-                    if(c.getCoorMapa(d4)=='M' || (c.getCoorMapa(d5)=='L' || c.getCoorMapa(d5)=='T')){
-                        direccion_inicial=5;
-                        nuevaCoordenada=d5;
-                    }
-                    else{
-                        if(c.getCoorMapa(d5)=='M' || (c.getCoorMapa(d6)=='L' || c.getCoorMapa(d6)=='T')){
-                            direccion_inicial=6;
-                            nuevaCoordenada=d6;
-                        }
-                        else{
-                            if(c.getCoorMapa(d6)=='M' || (c.getCoorMapa(d7)=='L' || c.getCoorMapa(d7)=='T')){
-                                direccion_inicial=7;
-                                nuevaCoordenada=d7;
-                            }
-                            else{
-                                direccion_inicial=0;
-                                nuevaCoordenada=d0;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    } 
-
+    
     vector<int> movimientos;
     movimientos.push_back(direccion_inicial);
 
@@ -117,101 +96,76 @@ int main(int argc, char *argv[]){
     Localidad p_nueva;
 
 
-
+    /*
     while(nuevaCoordenada.getFila()!=inicio.getLocalidad().getCoor().getFila() && nuevaCoordenada.getColumna()!=inicio.getLocalidad().getCoor().getColumna()){
+        cout<<"Siguiente direccion: "<<direccion_inicial<<endl;
+
         direccion_inicial=(direccion_inicial+5)%8;
         switch(direccion_inicial){
             case 0: 
                 direccion_inicial=vista0(c, inicio, nuevaCoordenada);
                 break;
             case 1:
+                direccion_inicial=vista1(c, inicio, nuevaCoordenada);
                 break;
             case 2:
+                direccion_inicial=vista2(c, inicio, nuevaCoordenada);
                 break;
             case 3:
+                direccion_inicial=vista3(c, inicio, nuevaCoordenada);
                 break;
             case 4:
+                direccion_inicial=vista4(c, inicio, nuevaCoordenada);
                 break;
             case 5:
+                direccion_inicial=vista5(c, inicio, nuevaCoordenada);
                 break;
             case 6:
+                direccion_inicial=vista6(c, inicio, nuevaCoordenada);
                 break;
             case 7: 
+                direccion_inicial=vista6(c, inicio, nuevaCoordenada);
                 break;
         }
         recorridoLocalidades+=1;
         movimientos.push_back(direccion_inicial);
         if(c.getCoorMapa(nuevaCoordenada)=='L'){
             for(unsigned int i=0;i<c.getLocalidades().size();i++){
-                if(nuevaCoordenada.getFila()==c.getLocalidades()[i].getCoor().getFila && nuevaCoordenada.getColumna()==c.getLocalidades().getCoor().getColumna()){
+                if(nuevaCoordenada.getFila()==c.getLocalidades()[i].getCoor().getFila() && nuevaCoordenada.getColumna()==c.getLocalidades()[i].getCoor().getColumna()){
                     p_nueva=c.getLocalidades()[i];
                 }
             }
-            cout<<p_anterior.getNombre()<<" "<<p_nueva.getNombre()<<" "<<recorrido<<endl;
+            cout<<p_anterior.getNombre()<<" "<<p_nueva.getNombre()<<" "<<recorridoLocalidades<<endl;
             p_anterior=p_nueva;
         }
     }
-
-    
+    */
 }
 
-int vista0(Coleccion &c, Provincia &inicio, Coordenadas &nuevaCoordenada){
-
-    Coordenadas d0(inicio.getLocalidad().getCoor().getFila(), inicio.getLocalidad().getCoor().getColumna()+1);
-    Coordenadas d1(inicio.getLocalidad().getCoor().getFila()+1, inicio.getLocalidad().getCoor().getColumna()+1);
-    Coordenadas d2(inicio.getLocalidad().getCoor().getFila()+1, inicio.getLocalidad().getCoor().getColumna());
-    Coordenadas d3(inicio.getLocalidad().getCoor().getFila()+1, inicio.getLocalidad().getCoor().getColumna()-1);
-    Coordenadas d4(inicio.getLocalidad().getCoor().getFila(), inicio.getLocalidad().getCoor().getColumna()-1);
-    Coordenadas d5(inicio.getLocalidad().getCoor().getFila()-1, inicio.getLocalidad().getCoor().getColumna()-1);
-    Coordenadas d6(inicio.getLocalidad().getCoor().getFila()-1, inicio.getLocalidad().getCoor().getColumna());
-    Coordenadas d7(inicio.getLocalidad().getCoor().getFila()-1, inicio.getLocalidad().getCoor().getColumna()+1);
-    int direccion_inicial=0;
-    //Coordenadas nuevaCoordenada;
-
-    if(c.getCoorMapa(d0)=='M' || (c.getCoorMapa(d1)=='L' || c.getCoorMapa(d1)=='T')){
-        direccion_inicial=1;
-        nuevaCoordenada=d1;
-    }
-    else{
-        if(c.getCoorMapa(d1)=='M' || (c.getCoorMapa(d2)=='L' || c.getCoorMapa(d2)=='T')){
-            direccion_inicial=2;
-            nuevaCoordenada=d2;
-        }
-        else{
-            if(c.getCoorMapa(d2)=='M' || (c.getCoorMapa(d3)=='L' || c.getCoorMapa(d3)=='T')){
-                direccion_inicial=3;
-                nuevaCoordenada=d3;
-            }
-            else{
-                if(c.getCoorMapa(d3)=='M' || (c.getCoorMapa(d4)=='L' || c.getCoorMapa(d4)=='T')){
-                    direccion_inicial=4;
-                    nuevaCoordenada=d4;
-                }
-                else{
-                    if(c.getCoorMapa(d4)=='M' || (c.getCoorMapa(d5)=='L' || c.getCoorMapa(d5)=='T')){
-                        direccion_inicial=5;
-                        nuevaCoordenada=d5;
-                    }
-                    else{
-                        if(c.getCoorMapa(d5)=='M' || (c.getCoorMapa(d6)=='L' || c.getCoorMapa(d6)=='T')){
-                            direccion_inicial=6;
-                            nuevaCoordenada=d6;
-                        }
-                        else{
-                            if(c.getCoorMapa(d6)=='M' || (c.getCoorMapa(d7)=='L' || c.getCoorMapa(d7)=='T')){
-                                direccion_inicial=7;
-                                nuevaCoordenada=d7;
-                            }
-                            else{
-                                direccion_inicial=0;
-                                nuevaCoordenada=d0;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    return direccion_inicial; 
+void cambiarCoordenadas(Coordenadas &d0, Coordenadas &d1, Coordenadas &d2, Coordenadas &d3, Coordenadas &d4, Coordenadas &d5, Coordenadas &d6, Coordenadas &d7, Coordenadas &ref){
+    d0=Coordenadas(ref.getFila(), ref.getColumna()+1);
+    d1=Coordenadas(ref.getFila()+1, ref.getColumna()+1);
+    d2=Coordenadas(ref.getFila()+1, ref.getColumna());
+    d3=Coordenadas(ref.getFila()+1, ref.getColumna()-1);
+    d4=Coordenadas(ref.getFila(), ref.getColumna()-1);
+    d5=Coordenadas(ref.getFila()-1, ref.getColumna()-1);
+    d6=Coordenadas(ref.getFila()-1, ref.getColumna());
+    d7=Coordenadas(ref.getFila()-1, ref.getColumna()+1);
 }
+
+vector<Coordenadas> creaVista(vector<Coordenadas> &v){
+    vector<Coordenadas> ret;
+    ret.push_back(v[0]);
+    ret.push_back(v[1]);
+    ret.push_back(v[2]);
+    ret.push_back(v[3]);
+    ret.push_back(v[4]);
+    ret.push_back(v[5]);
+    ret.push_back(v[6]);
+    ret.push_back(v[7]);
+}
+
+int vista0(Coleccion &c, Provincia &p, Coordenadas &coor){
+
+}
+
